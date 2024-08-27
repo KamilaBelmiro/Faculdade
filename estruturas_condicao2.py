83228eb61e961db1fc2253567b3c723438c02be9
KamilaBelmiro <contatokamilambelmiro@gmail.com>
2024-08-27 20:11:15 -0300

    adicao de arquivo

---
+++ estruturas_condicao2.py
@@ -0,0 +1,8 @@
+tempoExperiencia = 3
+if tempoExperiencia < 2:
+    print("Nivel de conhecimento Junior")
+elif tempoExperiencia > 2 < 5:
+    print("Nivel de conhecimento Pleno")  
+
+else:
+    print("Nivel de conhecimento Senior")
